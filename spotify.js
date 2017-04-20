5ab30777673ec6bcd9835fd523b6ddec48c709ab
Scylol <djd3dobbin@gmail.com>
2017-04-20 12:17:20 -0400

    initial commit

---
+++ spotify.js
@@ -0,0 +1,22 @@
+var getFromApi = function(endpoint, query={}) {
+    const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
+    Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
+    console.log(url);
+    return fetch(url).then(function(response) {
+        if (!response.ok) {
+            return Promise.reject(response.statusText);
+        }
+        return response.json();
+    });
+};
+
+
+var artist;
+var getArtist = function(name) {
+    console.log(name);
+    return getFromApi('search', query={q: name, limit: 1, type: 'artist'})
+    .then(item => {
+        artist = item.artists.items[0];
+        return artist;
+    })
+};
