725d900fbf8fc1393d8cf332e2aaba6da23d0b8b
Scylol <djd3dobbin@gmail.com>
2017-04-20 20:50:26 -0400

    fixed up promises

--- spotify.js
+++ spotify.js
@@ -1,7 +1,7 @@
 var getFromApi = function(endpoint, query={}) {
 	const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
 	Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
-    
+	
 	return fetch(url).then(function(response) {
 		if (!response.ok) {
 			return Promise.reject(response.statusText);
@@ -11,32 +11,31 @@ var getFromApi = function(endpoint, query={}) {
 };
 
 
-var artist;
 var getArtist = function(name) {
-    
-	return getFromApi('search', query={q: name, limit: 1, type: 'artist'})
-    .then(item => {
-	artist = item.artists.items[0];
-	return getFromApi(`artists/${artist.id}/related-artists`)
-    .then(item => { 
-	artist.related= item.artists;
-	let topTracksPromise = artist.related.map((artist) => {
-		return getFromApi(`artists/${artist.id}/top-tracks`, query = {country: 'US'});
+	var artist;
+	
+	return getFromApi('search', {q: name, limit: 1, type: 'artist'})
+	.then(item => {
+		artist = item.artists.items[0];
+		return getFromApi(`artists/${artist.id}/related-artists`);
+	})
+	.then(item => { 
+		artist.related = item.artists;
+		let topTracksPromise = artist.related.map((artist) => {
+			return getFromApi(`artists/${artist.id}/top-tracks`, {country: 'US'});
+		});
+		return Promise.all(topTracksPromise);
+	})	
+	.then(eachArtist => {
+		for(let i = 0; i < eachArtist.length; i++) {
+			artist.related[i].tracks = eachArtist[i].tracks;
+		}
+		return artist;
+	})
+	.catch(function(err) {
+		console.log(err);
 	});
-	var promise= Promise.all(topTracksPromise);
-
-	return promise
-    .then(eachArtist => {
-	for(let i = 0; i< eachArtist.length; i++) {
-		artist.related[i].tracks = eachArtist[i].tracks;
-	}
-	return artist;
-});
-})
-    .catch(function(err) {
-	console.log(err);
-});
-});
+	
 };
 
 
